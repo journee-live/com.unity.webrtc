b24672860f82068f5c10a87abc74e72bf7b0148b
kazuki matsumoto <karasusan@gmail.com>
2020-10-08 15:22:51 +0900

    Add RTCErrorException class

---
+++ Runtime/Scripts/RTCErrorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Unity.WebRTC
+{
+    public class RTCErrorException : Exception
+    {
+        private RTCError m_error;
+
+        internal RTCErrorException(ref RTCError error) : base(error.message)
+        {
+            m_error = error;
+        }
+        public RTCErrorType ErrorType { get { return m_error.errorType; } }
+    }
+}
